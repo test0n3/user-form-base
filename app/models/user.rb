5d30433178953f984fcc60d8be0efa2fa324803c
Carlos A <cayala.w@gmail.com>
2019-04-09 21:15:51 -0500

    Change redirection of server

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   validates :first_name, :last_name, length: { minimum: 2, maximum: 60 }
   # validates :last_name, length: { minimum: 2, maximum: 60 }
   validates :username, uniqueness: true, length: { minimum: 2, maximum: 20 }
-  validates :email, uniqueness: true, confirmation: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
+  validates :email, confirmation: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
   # validates :email_confirmation, presence: true
-  validates :password, confirmation: true, length: { minimum: 8 }, format: { with: /\A(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?!.*\s).{8,20}\Z/}
+  validates :password, length: { minimum: 8 }, confirmation: true, format: { with: /\A(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?!.*\s).{8,20}\Z/}
   # validates :password_confirmation, presence: true
   validates :password_hint, length: { maximum: 100 }
 
